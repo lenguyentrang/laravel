9d592f4d8ad230a64a4728db976a830d2eefe0f7
Le Nguyen Trang <lenguyentrang83@gmail.com>
2018-07-14 11:36:11 +0700

    create migration for the table.

---
+++ database/migrations/2018_07_14_040911_create_films_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('description')->nullable();
+            $table->date('release_date')->nullable();
+            $table->tinyInteger('rating')->nullable();
+            $table->bigInteger('ticket_price')->nullable();
+            $table->string('country')->nullable();
+            $table->string('genre')->nullable();
+            $table->string('photo')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
